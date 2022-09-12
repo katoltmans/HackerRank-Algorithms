80ca48d713fa28e190f444b55623dcd3efea5d12
katoltmans <katoltmans@msn.com>
2022-09-12 16:24:20 -0400

    Add "Strong Password" algorithm

--- Interview Preparation/week5.js
+++ Interview Preparation/week5.js
@@ -38,11 +38,54 @@ console.log(maxMin(3, [10, 100, 300, 200, 1000, 20, 30])); //expect 20
 console.log();
 
 {
-    /*  */
+    /*
+    Strong Password
+    
+    Louise joined a social networking site to stay in touch with her friends. 
+    The signup page required her to input a name and a password. 
+    However, the password must be strong. The website considers a password to be strong if it satisfies the following criteria:
+
+    -Its length is at least 6.
+    -It contains at least one digit.
+    -It contains at least one lowercase English character.
+    -It contains at least one uppercase English character.
+    -It contains at least one special character. The special characters are: !@#$%^&*()-+
+    */
 }
 
-console.log("");
+function minimumNumber(n, password) {
+    // Return the minimum number of characters to make the password strong
+    // Evaluate the length of the password
+    // Check for each requirement - if needed, add one
+    // Make sure the number of characters needed would be sufficient - if not, add required number
+    let addChar = 0;
 
+    if (!/[a-z]/.test(password)) {
+        addChar += 1;
+        console.log("Need lower case");
+    }
+    if (!/[A-Z]/.test(password)) {
+        addChar += 1;
+        console.log("Need upper case");
+    }
+    if (!/[0-9]/.test(password)) {
+        addChar += 1;
+        console.log("Need number");
+    }
+    if (!/[!@#$%^&*()--+)]/.test(password)) {
+        addChar += 1;
+        console.log("Need special character");
+    }
+    if (n + addChar < 6) {
+        console.log("PASSWORD:", password);
+        console.log("ADDCHAR", addChar);
+        addChar += 6 - (n + addChar);
+    }
+    return addChar;
+}
+
+console.log("Strong Password");
+console.log(minimumNumber("Ab1"));
 console.log();
 
 {
