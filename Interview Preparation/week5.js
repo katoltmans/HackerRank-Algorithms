6b1c825486445b56a3ba9e85bd5d95e9787590f2
katoltmans <katoltmans@msn.com>
2022-09-15 17:08:20 -0400

    Add "Sherlock And Array" algorithm

--- Interview Preparation/week5.js
+++ Interview Preparation/week5.js
@@ -89,11 +89,44 @@ console.log(minimumNumber("Ab1"));
 console.log();
 
 {
-    /*  */
+    /*
+    Sherlock and Array
+
+    Watson gives Sherlock an array of integers. 
+    His challenge is to find an element of the array such that the sum of all elements to the left is equal to the sum of all elements to the right.
+    */
 }
 
-console.log("");
+function balancedSums(arr) {
+    // sum the total of the array
+    // arrays with i value have values of non on each side - return YES
+    // arrays with 2 values cannot have an equal value on each side - return NO
+    // edge cases - the value to left of arr[0] and right or arr[arr.length-1] count as none
 
+    let arrTotal = arr.reduce((previous, current) => previous + current);
+    let arrSum = 0;
+
+    if (arr.length === 1) {
+        return "YES";
+    }
+
+    if (arr.length < 3) {
+        return "NO";
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arrSum === arrTotal - arrSum - arr[i]) {
+            return "YES";
+        }
+        arrSum += arr[i];
+    }
+
+    return "NO";
+}
+
+console.log("Sherlock And Array");
+console.log(balancedSums([1, 2, 3]));
+console.log(balancedSums([2, 0, 0, 0]));
 console.log();
 
 {
