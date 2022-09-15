24cf63890e0f9015bb8272af03ce987e20250c56
katoltmans <katoltmans@msn.com>
2022-09-15 17:18:50 -0400

    Add "Misere Nim" algorithm - (game of stones)

--- Interview Preparation/week5.js
+++ Interview Preparation/week5.js
@@ -130,11 +130,49 @@ console.log(balancedSums([2, 0, 0, 0]));
 console.log();
 
 {
-    /*  */
+    /*
+    Misère Nim
+
+    Two people are playing game of Misère Nim. The basic rules for this game are as follows:
+
+    The game starts with n piles of stones indexed from 0 to n-1. Each pile i (where 0<i<n) has s[i] stones.
+    The players move in alternating turns. During each move, the current player must remove one or more stones from a single pile.
+    The player who removes the last stone loses the game.
+    Given the value of n and the number of stones in each pile, determine whether the person who wins the game is the first or second person to move. 
+    If the first player to move wins, print First on a new line; otherwise, print Second. Assume both players move optimally.
+    
+    */
 }
 
-console.log("");
+function misereNim(s) {
+    // If each index only contains one stone, First will win even length arrays and Second will win odd length arrays
+    // Columns with multiple values need duplicates removed (choices that will cancel each other out). Use XOR to do this
+    let numChoices = s.length;
+    let total = s.reduce((a, b) => a + b);
+    let extraChoices = s.reduce((a, b) => a ^ b);
 
+    // console.log("NUMCHOICES:", numChoices);
+    // console.log("EXTRACHOICES:", extraChoices);
+    // console.log("TOTAL:", total);
+
+    if (numChoices === total) {
+        if (numChoices % 2 === 0) {
+            return "First";
+        } else {
+            return "Second";
+        }
+    }
+
+    if (extraChoices === 0) {
+        return "Second";
+    } else {
+        return "First";
+    }
+}
+
+console.log("Misère Nim");
+console.log(misereNim([2, 1, 3]));
+console.log(misereNim([9, 8, 4, 4, 4, 7]));
 console.log();
 
 {
