a9de4f8fd6f1f9d294e580928652cb76d4657982
katoltmans <katoltmans@msn.com>
2022-10-10 16:22:57 -0400

    Add "Counting Sort 1" algorithm

--- Interview Preparation/week2.js
+++ Interview Preparation/week2.js
@@ -110,6 +110,39 @@ console.log(diagonalDifference(matrix));
 console.log();
 
 {
+    /*
+    Counting Sort 1
+    Quicksort usually has a running time of n * log(n), but is there an algorithm that can sort even faster? 
+    In general, this is not possible. Most sorting algorithms are comparison sorts, i.e. they sort a list just by 
+    comparing the elements to one another. A comparison sort algorithm cannot beat n * log(n) (worst-case) running time, 
+    since n * log(n) represents the minimum number of comparisons needed to know where to place each element. 
+    */
+}
+
+function countingSort(arr) {
+    const arrayCounts = new Array(100).fill(0);
+
+    for (let i = 0; i < arr.length; i++) {
+        let index = arr[i];
+        arrayCounts[index] += 1;
+    }
+    return arrayCounts;
+}
+
+console.log("Counting Sort 1");
+console.log(
+    countingSort([
+        63, 54, 17, 78, 43, 70, 32, 97, 16, 94, 74, 18, 60, 61, 35, 83, 13, 56,
+        75, 52, 70, 12, 24, 37, 17, 0, 16, 64, 34, 81, 82, 24, 69, 2, 30, 61,
+        83, 37, 97, 16, 70, 53, 0, 61, 12, 17, 97, 67, 33, 30, 49, 70, 11, 40,
+        67, 94, 84, 60, 35, 58, 19, 81, 16, 14, 68, 46, 42, 81, 75, 87, 13, 84,
+        33, 34, 14, 96, 7, 59, 17, 98, 79, 47, 71, 75, 8, 27, 73, 66, 64, 12,
+        29, 35, 80, 78, 80, 6, 5, 24, 49, 82,
+    ])
+);
+console.log();
+
+{
     /*  */
 }
 
@@ -121,13 +154,17 @@ console.log();
     /*  */
 }
 
-{
-    /*  */
-}
+console.log("");
+
+console.log();
 
 {
     /*  */
 }
+
+console.log("");
+
+console.log();
 
 {
     /* 1
