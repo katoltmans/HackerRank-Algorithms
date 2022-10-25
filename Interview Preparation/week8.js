afbff31a155d93a30e7d1ecce4d09d6dc6731267
katoltmans <katoltmans@msn.com>
2022-10-24 20:17:47 -0400

    Add "Ice Cream Parlor" algo

--- Interview Preparation/week8.js
+++ Interview Preparation/week8.js
@@ -15,11 +15,35 @@ console.log("");
 console.log();
 
 {
-    /*  */
+    /*
+    Ice Cream Parlor
+
+    Two friends like to pool their money and go to the ice cream parlor. They always choose two distinct flavors and they spend all of their money.
+    Given a list of prices for the flavors of ice cream, select the two that will cost all of the money they have.
+        Example: m = 6 cost = [1,3,4,5,6]
+    The two flavors that cost 1 and 5 meet the criteria. Using 1-based indexing, they are at indices 1 and 4.
+    */
 }
 
-console.log("");
+function icecreamParlor(m, arr) {
+    // Loop the array and sum with the initial value to see if the total cost = m
+    // Break and return an array of those sorted values
+    let result = [];
 
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === m) {
+                result.push(i + 1, j + 1);
+                // console.log("RESULT:", result);
+                return result.sort((a, b) => a + b);
+            }
+        }
+    }
+}
+
+console.log("Ice Cream Parlor");
+console.log(icecreamParlor(4, [1, 4, 5, 3, 2])); // expect 1, 4
+console.log(icecreamParlor(9, [1, 3, 4, 6, 7, 9])); // expect 2, 4
 console.log();
 
 {
