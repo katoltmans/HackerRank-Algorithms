ca97bd3a1f25271094cd65f465db5423e9fa3ad7
katoltmans <katoltmans@msn.com>
2022-11-03 18:20:47 -0400

    Add "Arithmetic Operators", "Functions", "Let and Const", and "If Else" algos

--- 10 Days of JavaScript/basics.js
+++ 10 Days of JavaScript/basics.js
@@ -54,35 +54,130 @@ console.log(performOperation(17, 26.93, "Rocks!")); // expect 21, 30.93, HackerR
 console.log();
 
 {
-    /*  */
+    /*
+    Arithmetic Operators
+    
+    Complete the following functions in the editor below:
+        1. getArea(length, width): Calculate and return the area of a rectangle having sides length and width.
+        3. getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides length and width.
+    */
 }
 
-console.log("");
-console.log();
+// Calculate the area of a rectangle.
+function getArea(length, width) {
+    let area;
+    // Multiply length * width to calculate area
+    area = length * width;
+
+    return area;
+}
+
+//Calculate the perimeter of a rectangle.
+function getPerimeter(length, width) {
+    let perimeter;
+    // add 2*length + 2*width to calculate perimeter
+    perimeter = 2 * length + 2 * width;
+
+    return perimeter;
+}
+
+console.log("Arithmetic Operators");
+console.log(getArea(3, 4.5)); // expect 13.5
+console.log(getPerimeter(3, 4.5)); // expect 15
 console.log();
 
 {
-    /*  */
+    /*
+    Functions
+
+    Implement a function named factorial that has one parameter: an integer, n. It must return the value of n! (i.e., n factorial).
+    */
 }
 
-console.log("");
-console.log();
+//Create the function factorial here
+
+const factorial = (n) => {
+    let total = n;
+    let i = n - 1;
+
+    while (i > 0) {
+        total *= i;
+        i--;
+    }
+
+    return total;
+};
+
+console.log("Functions");
+console.log(factorial(4)); // expect 24
 console.log();
 
 {
-    /*  */
+    /*
+    Let and Const
+    
+    1. Declare a constant variable, PI, and assign it the value Math.PI. You will not pass this challenge unless the variable is declared as a constant and named PI (uppercase).
+    2. Read a number, r, denoting the radius of a circle from stdin.
+    3. Use PI and r to calculate the area and perimeter of a circle having radius .
+    4. Print area as the first line of output and print perimeter as the second line of output.
+    */
 }
 
-console.log("");
-console.log();
+function main(num) {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    let r = num; //parseFloat(readLine());
+
+    // Print the area of the circle:
+    console.log(PI * Math.pow(r, 2));
+
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r);
+    return;
+}
+
+console.log("Let and Const");
+console.log(main(2.6)); // expect 21.23716....., 16.33628....
 console.log();
 
 {
-    /*  */
+    /*
+    Conditional Statements: If-Else
+    
+    Complete the getGrade(score) function in the editor. It has one parameter: an integer, score, denoting the number of points Julia earned on an exam. 
+    It must return the letter corresponding to her grade according to the following rules:
+        If 25 < score <= 30, then grade = A.
+        If 20 < score <= 25, then grade = B.
+        If 15 < score <= 20, then grade = C.
+        If 10 < score <= 15, then grade = D.
+        If 5 < score <= 10, then grade = E.
+        If 0 < score <= 5, then grade = F.
+    */
 }
 
-console.log("");
-console.log();
+function getGrade(score) {
+    let grade;
+    // Determine grade based on score range
+
+    if (score > 25) {
+        grade = "A";
+    } else if (score > 20 && score <= 25) {
+        grade = "B";
+    } else if (score > 15 && score <= 20) {
+        grade = "C";
+    } else if (score > 10 && score <= 15) {
+        grade = "D";
+    } else if (score > 5 && score <= 10) {
+        grade = "E";
+    } else {
+        grade = "F";
+    }
+
+    return grade;
+}
+
+console.log("Conditional Statements: If-Else");
+console.log(getGrade(11)); // expect D
 console.log();
 
 {
