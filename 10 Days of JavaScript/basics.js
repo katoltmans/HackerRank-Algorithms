f2722da9de6b89bee8b1ceb71738c6dc262e4897
katoltmans <katoltmans@msn.com>
2022-11-07 15:33:18 -0500

    Add "Loops" and "Arrays" alogs

--- 10 Days of JavaScript/basics.js
+++ 10 Days of JavaScript/basics.js
@@ -181,17 +181,132 @@ console.log(getGrade(11)); // expect D
 console.log();
 
 {
-    /*  */
+    /*
+    Conditionals - Switch
+    
+    Complete the getLetter(s) function in the editor. It has one parameter: a string, , consisting of lowercase English alphabetic letters (i.e., a through z). It must return A, B, C, or D depending on the following criteria:
+        If the first character in string s is in the set {a, e, i, o, u}, then return A.
+        If the first character in string s is in the set {b, c, d, f, g}, then return B.
+        If the first character in string s is in the set {h, j, k, l, m}, then return C.
+        If the first character in string s is in the set {n, p, q, r, s, t, v, w, x, y, z}, then return D.
+        Hint: You can get the letter at some index i in s using the syntax s[i] or s.charAt(i).
+    */
 }
 
-console.log("");
-console.log();
+function getLetter(s) {
+    let letter;
+    // Check which case first character matches and return the appropriate letter
+
+    switch (s[0]) {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            letter = "A";
+            break;
+        case "b":
+        case "c":
+        case "d":
+        case "f":
+        case "g":
+            letter = "B";
+            break;
+        case "h":
+        case "j":
+        case "k":
+        case "l":
+        case "m":
+            letter = "C";
+            break;
+        case "n":
+        case "p":
+        case "q":
+        case "r":
+        case "s":
+        case "t":
+        case "u":
+        case "v":
+        case "w":
+        case "x":
+        case "y":
+        case "z":
+            letter = "D";
+            break;
+    }
+
+    return letter;
+}
+
+console.log("Conditionals - Switch");
+console.log(getLetter("adfgt")); // expect A
 console.log();
 
 {
-    /*  */
+    /*
+    Loops
+    
+    First, print each vowel in s on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in s.
+    Second, print each consonant (i.e., non-vowel) in s on a new line in the same order as it appeared in s.
+    */
 }
 
-console.log("");
+function vowelsAndConsonants(s) {
+    // print each vowel of a string to a new line followed by each consonant in order
+
+    let vowels = ["a", "e", "i", "o", "u"];
+    let consonents = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            console.log(s[i]);
+        } else {
+            consonents.push(s[i]);
+        }
+    }
+
+    for (let j = 0; j < consonents.length; j++) {
+        console.log(consonents[j]);
+    }
+    return;
+}
+
+console.log("Loops");
+console.log(vowelsAndConsonants("javascriptloops")); // expect a, a, i, o, o, j, v, s, c, r, p, t, l, p, s
 console.log();
+
+{
+    /*
+    Arrays
+    
+    Complete the getSecondLargest function in the editor below.
+    getSecondLargest has the following parameters:
+        int nums[n]: an array of integers
+    */
+}
+
+function getSecondLargest(nums) {
+    // Set max to the first value of the array
+    // If the next value is less than max, set second - otherwise replace max and set max to second
+    // iterate through the array, comparing values to max and second
+
+    let max = nums[0];
+    let secondLargest;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < max && nums[i] > secondLargest) {
+            secondLargest = nums[i];
+        }
+
+        if (nums[i] > max) {
+            secondLargest = max;
+            max = nums[i];
+        }
+    }
+    return secondLargest;
+}
+
+console.log("Arrays");
+console.log(getSecondLargest([2, 3, 6, 6, 5])); // expect 5
+console.log(getSecondLargest([-7, -3, -9, 1, -2, -15])); // expect -2
 console.log();
