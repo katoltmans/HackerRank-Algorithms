6c952eda5ad0a64cd3ce7c94b17657261f7e73d6
katoltmans <katoltmans@msn.com>
2022-11-08 14:09:23 -0500

    Add "Regular Expressions II" algo
    Match:
    * specific prefix followed by period with no spaces
    * only letters follow prefix

--- 10 Days of JavaScript/regularExpressions.js
+++ 10 Days of JavaScript/regularExpressions.js
@@ -26,11 +26,34 @@ console.log(regexVar("abcdo")); // expect false
 console.log();
 
 {
-    /*  */
+    /*
+    Regular Expressions II
+    
+    Complete the function in the editor below by returning a RegExp object, re, that matches any string s satisfying both of the following conditions:
+    String s starts with the prefix Mr., Mrs., Ms., Dr., or Er.
+    The remainder of string s (i.e., the rest of the string after the prefix) consists of one or more upper and/or lowercase English alphabetic letters (i.e., [a-z] and [A-Z]).
+    */
 }
 
-console.log("");
-console.log();
+function regexVar2(s) {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.',
+     * followed by one or more letters.
+     */
+
+    const re = /^((Mr|Mrs|Ms|Dr|Er)\.)[a-zA-Z]+$/.test(s);
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+console.log("Regular Expressions II");
+console.log(regexVar2("Mr.X")); // expect true
+console.log(regexVar2("Dr#Joseph")); // expect false
+console.log(regexVar2("Er .Abc")); // expect false
 console.log();
 
 {
