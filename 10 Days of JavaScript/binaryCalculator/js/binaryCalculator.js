626bf098e02c2815f6eab4afa0ae077d05d03e05
katoltmans <katoltmans@msn.com>
2022-12-08 18:17:19 -0500

    Added functionality to calculator

--- 10 Days of JavaScript/binaryCalculator/js/binaryCalculator.js
+++ 10 Days of JavaScript/binaryCalculator/js/binaryCalculator.js
@@ -15,8 +15,11 @@ function calculate() {
     let re = /\d+/;
     // Regex to get operator signs
     let reOpp = /\+|-|\*|\//;
-    let numbers = value.split(re);
-    let operators = value.split(reOpp);
+    let numbers = value.split(reOpp);
+    let operators = value.split(re);
+    operators = operators.join("");
+
+    console.log("NUMBERS:", numbers, "OPERATORS:", operators);
 
     // Return if nothing has been input into the calculator yet
     if (value === "") {
@@ -24,28 +27,45 @@ function calculate() {
     }
 
     // Set result equal to first binary number
-    let result = numbers[0];
-    let operator = null;
+    let result = parseInt(numbers[0], 2);
+    let operand = null;
 
-    // Switch cases to perform each operator function
-    for (let i = 0; i < equation.length; i++) {
-        switch (equation[i]) {
-            case "+":
-                break;
-            case "-":
-                result -= operand;
-                break;
-            case "*":
-                result *= operand;
-                break;
-            case "/":
-                result /= operand;
-                break;
-            default:
-                operand = parseInt(equation[i], 2);
-                console.log("OPERAND:", operand);
+    console.log("INITIAL RESULT:", result);
+
+    // Iterate through operator string and apply operation to the number one index beyond operator index
+    for (let i = 0; i < operators.length; i++) {
+        if (numbers[i + 1] === "") {
+            operand = null;
+        } else {
+            operand = numbers[i + 1]
+                ? (operand = parseInt(numbers[i + 1], 2))
+                : (operand = null);
+            console.log("OPERAND:", operand);
+
+            switch (operators[i]) {
+                case "+":
+                    result += operand;
+                    break;
+                case "-":
+                    result -= operand;
+                    break;
+                case "*":
+                    result *= operand;
+                    break;
+                case "/":
+                    result /= operand;
+                    break;
+                default:
+                    if (operand === null) {
+                        break;
+                    }
+            }
         }
     }
+
+    result = (result >>> 0).toString(2);
+    console.log("RESULT:", result);
+
     document.getElementById("res").innerText = result;
 }
 
